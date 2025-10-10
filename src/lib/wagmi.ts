00b18f2906ac4075c00a44081193ffcda78a43d2
David Furlong <dvfurlong@googlemail.com>
2025-10-10 11:44:33 +0200

    fix walletconnect id

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,10 +1,13 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, sepolia, base } from "wagmi/chains";
+import { base } from "wagmi/chains";
+
+if (!process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID) {
+  throw new Error("NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID is not set");
+}
 
 export const config = getDefaultConfig({
   appName: "ECP share",
-  projectId:
-    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "YOUR_PROJECT_ID", // Get this from https://cloud.walletconnect.com
-  chains: [base, mainnet, sepolia],
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  chains: [base],
   ssr: true,
 });
