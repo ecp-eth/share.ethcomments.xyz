9ed3f3a068ccae4cd2e6227e7eb5526389554dfb
Norman Xu <hi@norm.im>
2025-10-17 18:33:07 +0100

    feat: support native token fee required by channel

--- src/env.ts
+++ src/env.ts
@@ -4,5 +4,5 @@ import { never } from "./lib/utils";
  * Non public environment variables.
  */
 export const env = {
-  PINATA_JWT: process.env.PINATA_JWT ?? never(),
+  PINATA_JWT: process.env.PINATA_JWT ?? never("PINATA_JWT is required"),
 } as const;
--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,10 +1,17 @@
 import { publicEnv } from "@/publicEnv";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base } from "wagmi/chains";
+import * as chains from "wagmi/chains";
+import { getChainById, never } from "./utils";
+
+const selectedChain =
+  getChainById(
+    Number(publicEnv.NEXT_PUBLIC_CHAIN_ID) || 8453,
+    Object.values(chains)
+  ) ?? never("Chain not supported");
 
 export const config = getDefaultConfig({
   appName: "ECP share",
   projectId: publicEnv.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-  chains: [base],
+  chains: [selectedChain],
   ssr: true,
 });
